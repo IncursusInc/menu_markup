9233c59267f7e2fac1b2b491318bb276e3ca341f
sburkett <scott@incurs.us>
2016-08-19 08:07:05 -0400

    Formatting cleanup, etc.

--- src/Controller/MenuMarkupController.php
+++ src/Controller/MenuMarkupController.php
@@ -1,13 +1,5 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\menu_markup\Controller\MenuMarkupController.
- *
- * This controller handles the parsing of the config and the actual 
- * rebuilding of menu items.
- */
-
 namespace Drupal\menu_markup\Controller;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -16,105 +8,111 @@ use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\Entity\Query\QueryFactory;
 use Drupal\Component\Render\FormattableMarkup;
 
-class MenuMarkupController extends ControllerBase
-{
+/**
+ *
+ */
+class MenuMarkupController extends ControllerBase {
 
-    protected $configFactory;
-    protected $entityQuery;
-    private   $_markupOptions;
-    private   $_storedSettings;
-    private   $_links;
+  protected $configFactory;
+  protected $entityQuery;
+  private   $_markupOptions;
+  private   $_storedSettings;
+  private   $_links;
 
-    public function __construct( ConfigFactory $configFactory, QueryFactory $entityQuery, $links )
-    {
-        $this->configFactory = $configFactory;
-        $this->entityQuery = $entityQuery;
-        $this->_links = $links;
-    }
+  /**
+   *
+   */
+  public function __construct(ConfigFactory $configFactory, QueryFactory $entityQuery, $links) {
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function create(ContainerInterface $container) 
-    {
-        return new static(
-            $container->get('config.factory'),
-            $container->get('entity.query')
-        );
-    }
+    $this->configFactory = $configFactory;
+    $this->entityQuery = $entityQuery;
+    $this->_links = $links;
+  }
 
-    /**
-     *  Parse the configuration into something usable - one entry per line, so let's split it up
-     *
-     *  The configuration lines should be in the following format:
-     *
-     *  MENU_TITLE|REPLACEMENT_STRING|NODE_TYPE_MACHINE_NAME
-     *
-     *  Where:
-     *
-     *  MENU_TITLE = The name of the menu item (Home, News Archive, etc.)
-     *  REPLACEMENT_STRING = The string containing markup to replace the MENU_TITLE with
-     *  NODECOUNT = An optional content type machine name which can be used to generate a badge, etc.
-     *
-     *  @return none
-     */
-    public function parseMenuConfig() 
-    {
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
 
-         // Fetch the configuration for the menu_markup module
-         $this->_storedSettings = $this->configFactory->get('menu_markup.settings')->get('config');
+    return new static(
+        $container->get('config.factory'),
+        $container->get('entity.query')
+    );
+  }
 
-         $this->_markupOptions = array();
-         $lines = preg_split('/\r\n|[\r\n]/', $this->_storedSettings);
+  /**
+   * Parse the configuration into something usable - one entry per line, so let's split it up.
+   *
+   *  The configuration lines should be in the following format:
+   *
+   *  MENU_TITLE|REPLACEMENT_STRING|NODE_TYPE_MACHINE_NAME
+   *
+   *  Where:
+   *
+   *  MENU_TITLE = The name of the menu item (Home, News Archive, etc.)
+   *  REPLACEMENT_STRING = The string containing markup to replace the MENU_TITLE with
+   *  NODECOUNT = An optional content type machine name which can be used to generate a badge, etc.
+   *
+   * @return none
+   */
+  public function parseMenuConfig() {
 
-        foreach ($lines as $line) {
-            $tmp = explode('|', $line);
+    // Fetch the configuration for the menu_markup module.
+    $this->_storedSettings = $this->configFactory->get('menu_markup.settings')->get('config');
 
-            if (count($tmp) > 0) {
-                $this->_markupOptions[ $tmp[0] ]['menuTitle'] = $tmp[1];
-                if (isset($tmp[2])) {
-                     $this->_markupOptions[ $tmp[0] ]['nodeType'] = $tmp[2];
-                }
-            }
+    $this->_markupOptions = array();
+    $lines = preg_split('/\r\n|[\r\n]/', $this->_storedSettings);
+
+    foreach ($lines as $line) {
+      $tmp = explode('|', $line);
+
+      if (count($tmp) > 0) {
+        $this->_markupOptions[$tmp[0]]['menuTitle'] = $tmp[1];
+        if (isset($tmp[2])) {
+          $this->_markupOptions[$tmp[0]]['nodeType'] = $tmp[2];
         }
-
+      }
     }
 
-    /**
-     *  Rebuild the menu links!
-     *
-     *  @return array
-     */
-    public function rebuildMenuLinks()
-    {
-        // Now, let's rebuild the menu links
-        foreach ($this->_links as $index => $link) {
-            if (@array_key_exists($link['title'], $this->_markupOptions)) {
-                $translatedMenuTitle = $this->t($link['title']);
-                $menuTitleStr = (string) $translatedMenuTitle;
+  }
 
-                // Do we have a badge count here?
-                if (@$this->_markupOptions[$link['title']]['nodeType'] ) {
-                    $query = $this->entityQuery->get('node')
+  /**
+   * Rebuild the menu links!
+   *
+   * @return array
+   */
+  public function rebuildMenuLinks() {
+
+    // Now, let's rebuild the menu links.
+    foreach ($this->_links as $index => $link) {
+      if (@array_key_exists($link['title'], $this->_markupOptions)) {
+        $translatedMenuTitle = $this->t($link['title']);
+        $menuTitleStr = (string) $translatedMenuTitle;
+
+        // Do we have a badge count here?
+        if (@$this->_markupOptions[$link['title']]['nodeType']) {
+          $query = $this->entityQuery->get('node')
                       ->condition('type', $this->_markupOptions[$link['title']]['nodeType'])
                       ->condition('status', 1);
 
-                    $nids = $query->execute();
-                    $nodeType = count($nids);
-                } else {
-                    $nodeType = '';
-                }
-
-                // Token replacement
-                $replacementString = $this->_markupOptions[$link['title']]['menuTitle'];
-                $replacementString = preg_replace('/\{\{\s*title\s*\}\}/', $menuTitleStr, $replacementString);
-                $replacementString = preg_replace('/\{\{\s*nodeCount\s*\}\}/', $nodeType, $replacementString);
-
-                // This is where the magic happens - convert it!
-                $this->_links[$index]['title'] = new FormattableMarkup($replacementString, array());
-            }
+          $nids = $query->execute();
+          $nodeType = count($nids);
         }
-    
-        return $this->_links;
+        else {
+          $nodeType = '';
+        }
+
+        // Token replacement.
+        $replacementString = $this->_markupOptions[$link['title']]['menuTitle'];
+        $replacementString = preg_replace('/\{\{\s*title\s*\}\}/', $menuTitleStr, $replacementString);
+        $replacementString = preg_replace('/\{\{\s*nodeCount\s*\}\}/', $nodeType, $replacementString);
+
+        // This is where the magic happens - convert it!
+        $this->_links[$index]['title'] = new FormattableMarkup($replacementString, array());
+      }
     }
+
+    return $this->_links;
+  }
+
 }
--- src/Form/ConfigureForm.php
+++ src/Form/ConfigureForm.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * @file
- * Contains \Drupal\menu_markup\Form\ConfigureForm.
- */
 
 namespace Drupal\menu_markup\Form;
 
@@ -12,80 +8,80 @@ use Drupal\Core\Form\FormStateInterface;
 /**
  * Configure form.
  */
-class ConfigureForm extends ConfigFormBase
-{
-    /**
-     * {@inheritdoc}
-     *
-     * @return string
-     */
-    public function getFormId() 
-    {
-        return 'menu_markup_configure_form';
-    }
+class ConfigureForm extends ConfigFormBase {
 
-    /** 
-     * {@inheritdoc}
-     *
-     * @return array
-     */
-    protected function getEditableConfigNames() 
-    {
-        return [
-        'menu_markup.settings',
-        ];
-    }
+  /**
+   * {@inheritdoc}
+   *
+   * @return string
+   */
+  public function getFormId() {
 
-    /**
-     * {@inheritdoc}
-     *
-     * @param array                                $form
-     * @param \Drupal\Core\Form\FormStateInterface $form_state
-     *
-     * @return mixed
-     */
-    public function buildForm(array $form, FormStateInterface $form_state) 
-    {
+    return 'menu_markup_configure_form';
+  }
 
-        $config = $this->config('menu_markup.settings');
+  /**
+   * {@inheritdoc}
+   *
+   * @return array
+   */
+  protected function getEditableConfigNames() {
 
-        $form['menu_markup_configuration'] = array(
-        '#type' => 'textarea',
-        '#title' => t('Menu Markup Configuration'),
-        '#required' => true,
-        '#rows' => 10,
-        '#default_value' => $config->get('config'),
-        '#description' => t('Enter line values in the following format:  MENU TITLE|OPEN MARKUP|CLOSE MARKUP|OPTIONAL_CONTENT_TYPE_MACHINENAME'),
-        );
+    return [
+      'menu_markup.settings',
+    ];
+  }
 
-        return parent::buildForm($form, $form_state);
-    }
+  /**
+   * {@inheritdoc}
+   *
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *
+   * @return mixed
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
 
-    /**
-     * {@inheritdoc}
-     *
-     * @param array                                $form
-     * @param \Drupal\Core\Form\FormStateInterface $form_state
-     *
-     * @return none
-     */
-    public function validateForm(array &$form, FormStateInterface $form_state) 
-    {
-    }
+    $config = $this->config('menu_markup.settings');
 
-    /**
-     * {@inheritdoc}
-     *
-     * @param array                                $form
-     * @param \Drupal\Core\Form\FormStateInterface $form_state
-     *
-     * @return none
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state) 
-    {
-        $config = $this->config('menu_markup.settings');
-        $config->set('config', $form_state->getValue('menu_markup_configuration'))->save();
+    $form['menu_markup_configuration'] = array(
+      '#type' => 'textarea',
+      '#title' => t('Menu Markup Configuration'),
+      '#required' => TRUE,
+      '#rows' => 10,
+      '#default_value' => $config->get('config'),
+      '#description' => t('Enter line values in the following format:  MENU TITLE|OPEN MARKUP|CLOSE MARKUP|OPTIONAL_CONTENT_TYPE_MACHINENAME'),
+    );
 
-        parent::submitForm($form, $form_state);
-    }
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *
+   * @return none
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @param array $form
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *
+   * @return none
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $config = $this->config('menu_markup.settings');
+    $config->set('config', $form_state->getValue('menu_markup_configuration'))->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
 }
